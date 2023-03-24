ccd1f29b175e363c590e9342d04f6d76d32eefd7
dinasoa <hei.dinasoa@gmail.com>
2023-03-24 13:12:37 +0300

    chore: order getCourses

--- src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
@@ -3,11 +3,13 @@ package school.hei.haapi.endpoint.rest.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
 import school.hei.haapi.endpoint.rest.model.Course;
+import school.hei.haapi.endpoint.rest.model.OrderDirection;
 import school.hei.haapi.model.BoundedPageSize;
 import school.hei.haapi.model.PageFromOne;
 import school.hei.haapi.service.CourseService;
@@ -26,10 +28,12 @@ public class CourseController {
       @RequestParam(name = "name" ,required = false) String name,
       @RequestParam(name = "teacher_first_name" ,required = false) String teacherFirstName,
       @RequestParam(name = "teacher_last_name" ,required = false) String teacherLastName,
-      @RequestParam(name = "credits", required = false) Integer credits
-  ) {
+      @RequestParam(name = "credits", required = false) Integer credits,
+      @RequestParam(name = "creditsOrder", required = false) OrderDirection creditsOrder,
+      @RequestParam(name = "codeOrder", required = false) OrderDirection codeOrder
+      ) {
     return courseService.getAllCourses(page, pageSize, code, name, teacherFirstName,
-            teacherLastName, credits).stream().map(courseMapper::toRest)
+            teacherLastName, credits, creditsOrder, codeOrder).stream().map(courseMapper::toRest)
         .collect(Collectors.toUnmodifiableList());
   }
 }
